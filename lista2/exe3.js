f721496c17b675f72e6655ac4320e9ca0a549eea
Rafael <kumayshiro77@gmail.com>
2020-08-25 22:05:19 -0300

    exe3 lista 2 função de cadastro ok

--- lista2/exe3.js
+++ lista2/exe3.js
@@ -1,12 +1,17 @@
 main()
 function main(){
     var population = []
-
     do{
-        var option = Number(prompt("Digite conforme o desejado: \n [1]. req1 \n [2]. req2 \n [3]. req3 \n [4]. req4 \n [5]. req5 \n [6]. Sair do programa"))
+        var option = Number(prompt(`Digite conforme o desejado: 
+        [1]. Cadastrar novo habitante 
+        [2]. req2 
+        [3]. req3
+        [4]. req4
+        [5]. req5 
+        [6]. Sair do programa`))
         switch(option){
             case 1:
-                request1(population)
+                register(population)
                 break
             case 2:
                 request2(population)
@@ -27,4 +32,13 @@ function main(){
                 alert("Opção invalida")
         }
     }while(option != 6)
+}
+function register(arr){
+    var person = new Object()
+    person.gen = prompt("Gênero: ").toUpperCase()
+    person.h = Number(prompt("Altura: "))
+    person.age = Number(prompt("Idade: "))
+    person.eyes = prompt("Cor dos Olhos: A - Azul; V - Verde; C - Castanho")
+    arr.push(person)
+    console.log(arr)
 }
